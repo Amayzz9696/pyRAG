aac6758a6ad73585fcff1554b1194ff612466af2
zhangzhao <1137309182@qq.com>
2026-02-05 01:34:44 +0800

    feat: 添加命令行参数和路径输出功能
    在脚本中添加导入sys模块的argv和path，并增加命令行参数遍历和Python路径输出功能，方便调试和查看运行环境信息

--- 2.py
+++ 2.py
@@ -1,3 +1,5 @@
+from sys import argv,path
+
 #判断标识符是否合法
 def is_valid_identifier(name):
     try:
@@ -12,4 +14,10 @@ if __name__ == '__main__':
     # print(2**32)
     print(is_valid_identifier("2var"))
     print(is_valid_identifier("var2"))
+
+
+    print('命令行参数')
+    for arg in argv:
+        print(arg)
+    print('/n python路径为:',path) # 因为已经导入path成员，所以此处引用时不需要加sys.path
 
